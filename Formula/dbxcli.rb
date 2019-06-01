7afb42d1c1bbcf732c823005aca755fc56c29c0f
idleberg <git@idleberg.com>
2019-06-01 11:29:52 +0200

    add Linux support

--- Formula/dbxcli.rb
+++ Formula/dbxcli.rb
@@ -1,11 +1,20 @@
 class Dbxcli < Formula
   desc "A command line client for Dropbox built using the Go SDK"
   homepage "https://github.com/dropbox/dbxcli"
-  url "https://github.com/dropbox/dbxcli/releases/download/v3.0.0/dbxcli-darwin-amd64"
-  sha256 "1149a2aa6a89829c6d540d04cc1db8cf5bb27e3d8b0ec6b32d830a6818bd7573"
+
+  if OS.mac?
+    $os = "darwin"
+    checksum = "1149a2aa6a89829c6d540d04cc1db8cf5bb27e3d8b0ec6b32d830a6818bd7573"
+  else
+    $os = "linux"
+    checksum = "3ff6e29675095063a5ba7101115cf7f809f3deddf14998ac61bf7a0330d736cb"
+  end
+
+  url "https://github.com/dropbox/dbxcli/releases/download/v3.0.0/dbxcli-#{$os}-amd64"
+  sha256 checksum
 
   def install
-    system "mv dbxcli-darwin-amd64 dbxcli"
+    system "mv dbxcli-#{$os}-amd64 dbxcli"
     bin.install "dbxcli"
   end
 
