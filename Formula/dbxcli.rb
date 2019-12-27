596531f7539a65916ae88fc29664b3a5c89e8a28
Jan T. Sott <jan@idleberg.com>
2019-12-27 22:47:17 +0100

    Use class variable

--- Formula/dbxcli.rb
+++ Formula/dbxcli.rb
@@ -3,18 +3,18 @@ class Dbxcli < Formula
   homepage "https://github.com/dropbox/dbxcli"
 
   if OS.mac?
-    $os = "darwin"
+    @@os = "darwin"
     checksum = "1149a2aa6a89829c6d540d04cc1db8cf5bb27e3d8b0ec6b32d830a6818bd7573"
   else
-    $os = "linux"
+    @@os = "linux"
     checksum = "3ff6e29675095063a5ba7101115cf7f809f3deddf14998ac61bf7a0330d736cb"
   end
 
-  url "https://github.com/dropbox/dbxcli/releases/download/v3.0.0/dbxcli-#{$os}-amd64"
+  url "https://github.com/dropbox/dbxcli/releases/download/v3.0.0/dbxcli-#{@@os}-amd64"
   sha256 checksum
 
   def install
-    system "mv dbxcli-#{$os}-amd64 dbxcli"
+    system "mv dbxcli-#{@@os}-amd64 dbxcli"
     bin.install "dbxcli"
   end
 
