9f392e14636397566be320d0088c0fae631bc451
Yana Agun Siswanto <yana.developer@gmail.com>
2019-12-28 01:40:39 +0700

    Remove usage of global variable

--- Formula/dbxcli.rb
+++ Formula/dbxcli.rb
@@ -1,20 +1,26 @@
 class Dbxcli < Formula
   desc "A command line client for Dropbox built using the Go SDK"
   homepage "https://github.com/dropbox/dbxcli"
+  
+  def self.os
 
+  end
   if OS.mac?
-    $os = "darwin"
+    url "https://github.com/dropbox/dbxcli/releases/download/v3.0.0/dbxcli-darwin-amd64"
     checksum = "1149a2aa6a89829c6d540d04cc1db8cf5bb27e3d8b0ec6b32d830a6818bd7573"
   else
-    $os = "linux"
+    url "https://github.com/dropbox/dbxcli/releases/download/v3.0.0/dbxcli-linux-amd64"
     checksum = "3ff6e29675095063a5ba7101115cf7f809f3deddf14998ac61bf7a0330d736cb"
   end
 
-  url "https://github.com/dropbox/dbxcli/releases/download/v3.0.0/dbxcli-#{$os}-amd64"
   sha256 checksum
 
   def install
-    system "mv dbxcli-#{$os}-amd64 dbxcli"
+    if OS.mac?
+      system "mv dbxcli-darwin-amd64 dbxcli"
+    else
+      system "mv dbxcli-linux-amd64 dbxcli"
+    end
     bin.install "dbxcli"
   end
 
