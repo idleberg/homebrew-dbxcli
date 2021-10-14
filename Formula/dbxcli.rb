f6336edd5039fc97c2e1e5e6cbbbf019c515e18e
Jan T. Sott <git@idleberg.com>
2021-10-15 00:40:28 +0200

    address linter warnings

--- Formula/dbxcli.rb
+++ Formula/dbxcli.rb
@@ -1,11 +1,11 @@
 class Dbxcli < Formula
-  desc "A command line client for Dropbox built using the Go SDK"
+  desc "Command-line client for Dropbox built using the Go SDK"
   homepage "https://github.com/dropbox/dbxcli"
 
   if OS.mac?
     @@os = "darwin"
     checksum = "1149a2aa6a89829c6d540d04cc1db8cf5bb27e3d8b0ec6b32d830a6818bd7573"
-  else
+  elsif OS.linux?
     @@os = "linux"
     checksum = "3ff6e29675095063a5ba7101115cf7f809f3deddf14998ac61bf7a0330d736cb"
   end
@@ -14,7 +14,7 @@ class Dbxcli < Formula
   sha256 checksum
 
   def install
-    system "mv dbxcli-#{@@os}-amd64 dbxcli"
+    mv "dbxcli-#{@@os}-amd64", "dbxcli"
     bin.install "dbxcli"
   end
 
